4ec9d1504095b3fbfa73aba22d67cead599669a2
Bart Heyrman <bartheyrman22@gmail.com>
2019-07-02 21:00:04 +0200

    Modification: Add hardcoded RPR entry.

--- htdocs/core/lib/pdf.lib.php
+++ htdocs/core/lib/pdf.lib.php
@@ -410,7 +410,12 @@ function pdf_build_address($outputlangs, $sourcecompany, $targetcompany = '', $t
 				// EMail
 				if ($sourcecompany->email) $stringaddress .= ($stringaddress ? "\n" : '').$outputlangs->transnoentities("Email").": ".$outputlangs->convToOutputCharset($sourcecompany->email);
 				// Web
-				if ($sourcecompany->url) $stringaddress .= ($stringaddress ? "\n" : '').$outputlangs->transnoentities("Web").": ".$outputlangs->convToOutputCharset($sourcecompany->url);
+				if ($sourcecompany->url) {
+    			    $stringaddress .= ($stringaddress ? "\n" : '' ).$outputlangs->transnoentities("Web").": ".$outputlangs->convToOutputCharset($sourcecompany->url);
+                }
+                // RPR
+                $stringaddress .= ($stringaddress ? "\n" : '' ) . 'RPR' . ': ' . 'Gent, Afdeling Dendermonde';
+				}
 			}
 			// Intra VAT
 			if (!empty($conf->global->MAIN_TVAINTRA_IN_SOURCE_ADDRESS))
